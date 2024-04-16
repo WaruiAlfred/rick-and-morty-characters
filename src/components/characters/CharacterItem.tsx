41f60861088a76df91a55d71283464c8e977cef7
WaruiAlfred <alfredwarui17@gmail.com>
2024-04-16 19:01:18 +0300

    implement character modal

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,32 +1,48 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import CharacterModal from "./CharacterModal";
+import CharacterStatusIndicator from "./CharacterStatusIndicator";
 
 interface CharacterItemProps {
   character: any;
 }
 
 const CharacterItem: FC<CharacterItemProps> = ({ character }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="flex flex-col gap-2 bg-gray-100 shadow-md rounded-lg  hover:cursor-pointer overflow-hidden">
       <img
         src={character.image}
         alt={character.name}
         className="w-full h-full object-cover rounded-lg hover:scale-105 transition-all duration-300 ease-in-out "
+        onClick={handleOpenModal}
       />
       <div className="flex flex-col gap-2 text-center py-3">
-        <h1 className="text-2xl font-bold text-slate-800">{character.name}</h1>
+        <h1
+          className="text-2xl font-bold text-slate-800"
+          onClick={handleOpenModal}
+        >
+          {character.name}
+        </h1>
         <p className="text-lg text-slate-700 flex flex-row items-center justify-center">
-          <span
-            className={`w-2 h-2 rounded-full mr-1 ${
-              character.status === "Alive"
-                ? "bg-green-500"
-                : character.status === "Dead"
-                ? "bg-red-500"
-                : "bg-gray-500"
-            }`}
-          />
+          <CharacterStatusIndicator status={character.status} />
           {character.status} - {character.species}
         </p>
       </div>
+      {open && (
+        <CharacterModal
+          open={open}
+          onClose={handleCloseModal}
+          characterId={character.id}
+        />
+      )}
     </div>
   );
 };
--- src/components/characters/CharacterModal.tsx
+++ src/components/characters/CharacterModal.tsx
@@ -2,7 +2,6 @@ import Modal from "@mui/material/Modal";
 import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
 import { fetchCharacter } from "../../api/fetchCharacters";
-import EmptyResult from "../EmptyResult";
 import CharacterStatusIndicator from "./CharacterStatusIndicator";
 
 interface CharacterModalProps {
@@ -21,7 +20,7 @@ const CharacterModal: FC<CharacterModalProps> = ({
     queryFn: () => fetchCharacter(characterId),
   });
 
-  if (!data) return <EmptyResult />;
+  // if (!data) return <EmptyResult />;
 
   return (
     <Modal
@@ -30,49 +29,50 @@ const CharacterModal: FC<CharacterModalProps> = ({
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <div className="w-1/2 h-auto md:h-2/5 flex flex-col md:flex-row bg-gray-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg">
-        <div className="w-full md:w-3/5 h-full">
-          <img
-            src={data.image}
-            alt={data.name}
-            className="w-full h-full object-fill rounded-lg"
-          />
-        </div>
-        <div className="h-1/4 w-full p-4 flex flex-col gap-3">
-          <h1 className="text-2xl font-bold text-slate-800">{data.name}</h1>
-          <p className="text-lg text-slate-700 flex flex-row items-center">
-            <CharacterStatusIndicator status={data.status} />
-            {data.status} - {data.species}
-          </p>
-          <p className="flex flex-col gap-1">
-            <span className="italic text-slate-600">Gender</span>
-            <span className="text-slate-800 text-lg">{data.gender}</span>
-          </p>
-          <p className="flex flex-col gap-1">
-            <span className="italic text-slate-600">Origin</span>
-            <span className="text-slate-800 text-lg">{data.origin.name}</span>
-          </p>
-          <p className="flex flex-col gap-1">
-            <span className="italic text-slate-600">
-              Number of episodes appeared
-            </span>
-            <span className="text-slate-800 text-lg">
-              {data.episode.length}
-            </span>
-          </p>
-        </div>
-        {isFetching && (
-          <p className="text-2xl font-bold text-slate-700 text-center">
-            Loading...
-          </p>
-        )}
-        {isError && (
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="text-2xl font-bold text-slate-700">Error</h1>
-            <p className="text-lg text-slate-700">{error?.message}</p>
+      {isFetching ? (
+        <p className="text-2xl font-bold text-slate-700 text-center">
+          Loading...
+        </p>
+      ) : (
+        <div className="w-3/4 md:w-1/2 h-auto md:h-2/5 flex flex-col md:flex-row bg-gray-200 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg">
+          <div className="w-full md:w-3/5 h-full">
+            <img
+              src={data.image}
+              alt={data.name}
+              className="w-full h-full object-fill rounded-lg"
+            />
           </div>
-        )}
-      </div>
+          <div className="h-1/4 w-full p-4 flex flex-col gap-3">
+            <h1 className="text-2xl font-bold text-slate-800">{data.name}</h1>
+            <p className="text-lg text-slate-700 flex flex-row items-center">
+              <CharacterStatusIndicator status={data.status} />
+              {data.status} - {data.species}
+            </p>
+            <p className="flex flex-col gap-1">
+              <span className="italic text-slate-600">Gender</span>
+              <span className="text-slate-800 text-lg">{data.gender}</span>
+            </p>
+            <p className="flex flex-col gap-1">
+              <span className="italic text-slate-600">Origin</span>
+              <span className="text-slate-800 text-lg">{data.origin.name}</span>
+            </p>
+            <p className="flex flex-col gap-1">
+              <span className="italic text-slate-600">
+                Number of episodes appeared
+              </span>
+              <span className="text-slate-800 text-lg">
+                {data.episode.length}
+              </span>
+            </p>
+          </div>
+          {isError && (
+            <div className="flex flex-col items-center justify-center gap-4">
+              <h1 className="text-2xl font-bold text-slate-700">Error</h1>
+              <p className="text-lg text-slate-700">{error?.message}</p>
+            </div>
+          )}
+        </div>
+      )}
     </Modal>
   );
 };
